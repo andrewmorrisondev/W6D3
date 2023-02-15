b97b921ed29656e3905beed42979bc116066720a
andy <morrison.andrew422@gmail.com>
2023-02-15 17:21:03 -0500

    phase III

--- practice-for-ch-rails-first-routes-long-practice/route_demo/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice/route_demo/app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
+    @users = User.all
+    render json: @users
   end
 
   def create
-    render json: params
+    # debugger
+    user = User.new(params.require(:user).permit(:name, :email))
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 422
+    end
   end
 
   def show
