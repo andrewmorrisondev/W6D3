ebe51ea7bd1fdc22eca5248fd124c6b5867033f1
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-02-16 11:23:17 -0500

    phase4

--- practice-for-ch-rails-first-routes-long-practice/route_demo/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice/route_demo/app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def create
     # debugger
-    user = User.new(params.require(:user).permit(:name, :email))
+    user = User.new(user_params)
     if user.save
       render json: user
     else
@@ -15,6 +15,30 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: params
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_url(@user.id)
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy 
+      render json: "user deleted"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 end
